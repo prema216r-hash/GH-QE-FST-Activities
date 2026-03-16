8113938db623ce8d7414c16bc1975f708331346d
prema216r-hash <prema216r@gmail.com>
2026-03-16 16:44:07 +0530

    Add files via upload

---
+++ TestNG/Activities/TestNG/testNG_act4_2.java
@@ -0,0 +1,9 @@
+package TestNG;
+import org.testng.annotations.Test;
+
+public class testNG_act4_2  {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
