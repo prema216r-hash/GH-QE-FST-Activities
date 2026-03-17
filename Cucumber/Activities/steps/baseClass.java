ae1617e3cd1e5d4628b7f977fca75762aa2830c5
prema216r-hash <prema216r@gmail.com>
2026-03-17 14:37:58 +0530

    Add files via upload

---
+++ Cucumber/Activities/steps/baseClass.java
@@ -0,0 +1,10 @@
+package steps;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class baseClass {
+    // Create a new instance of the Firefox driver
+    static WebDriver driver;
+    static WebDriverWait wait;
+}
---
+++ Cucumber/Activities/testRunner/Activities4Runner.java
@@ -0,0 +1,19 @@
+// package testRunner;
+// import org.junit.platform.suite.api.Suite;
+// import org.junit.platform.suite.api.IncludeEngines;
+// import org.junit.platform.suite.api.ConfigurationParameter;
+// import org.junit.platform.suite.api.SelectClasspathResource;
+
+// import io.cucumber.junit.platform.engine.Constants;
+
+// @Suite
+// @IncludeEngines("cucumber")
+// @SelectClasspathResource("features")
+// @ConfigurationParameter(
+//   key = Constants.GLUE_PROPERTY_NAME, value = "steps"
+// )
+// @ConfigurationParameter(
+//   key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@Activity4"
+// )
+
+// public class Activities4Runner {}
